326d655c989664c180c92614b0b2f2492dcdb867
Archie Lee <archielee@appier.com>
2016-09-13 13:58:04 +0800

    Convert tabs to spaces

--- main-electron.js
+++ main-electron.js
@@ -10,36 +10,35 @@ require('electron-debug')();
 let mainWindow;
 
 function onClosed() {
-	// dereference the window
-	// for multiple windows store them in an array
-	mainWindow = null;
+  // dereference the window
+  // for multiple windows store them in an array
+  mainWindow = null;
 }
 
 function createMainWindow() {
-	const win = new electron.BrowserWindow({
-		width: 600,
-		height: 400
-	});
+  const win = new electron.BrowserWindow({
+    width: 600,
+    height: 400
+  });
 
-	win.loadURL(`http://localhost:3000/`);
-	win.on('closed', onClosed);
+  win.loadURL(`http://localhost:3000/`);
+  win.on('closed', onClosed);
 
-	return win;
+  return win;
 }
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
+  if (!mainWindow) {
+    mainWindow = createMainWindow();
+  }
 });
 
 app.on('ready', () => {
-	mainWindow = createMainWindow();
+  mainWindow = createMainWindow();
 });
-
